ecc87d46f9d0f351f015317b48760fe5be76a5b0
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-10-04 21:57:01 -0300

    renderizar receitas

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+receitas = {
+    1:  'Lasanha',
+    2: 'Sorvete',
+    3: 'Sopa',
+    4: 'Bola de Chocolate'
+}
+
+dados ={
+    'nome_das_receitas' : receitas
+}
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',dados)
 
 
 def receita(request):
