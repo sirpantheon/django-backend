814b1d9b8a980f684dd503464a51aebcc643f9fe
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-10-05 23:10:43 -0300

    migrations & models

--- jp_receita/settings.py
+++ jp_receita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'jp_receita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jp_receitas',
+        'USER': 'postgres',
+        'PASSWORD': '22@maximuzz',
+        'HOST': 'localhost',
     }
 }
 
